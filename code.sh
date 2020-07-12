152316d80c6cb081eccffee7b4c6a548fa13a74d
anurag08-git <64473339+anurag08-git@users.noreply.github.com>
2020-07-12 12:08:58 +0530

    code

---
+++ code.sh
@@ -0,0 +1,9 @@
+if kubectl get pods | grep httpd
+then
+echo "service is running"
+else
+kubectl create -f /mycode/http-pod.yaml
+sleep 20
+fi
+POD=$(kubectl get pod -l app=httpd -o jsonpath="{.items[0].metadata.name}")
+kubectl cp /mycode/*.html $POD:/var/www/html/
